3fb94f1df922d634f1e5e98abb54d6c6525d711e
Fola <jimiakinwale@icloud.com>
2022-08-08 14:47:41 +0100

    inital design of landing page without pictures or stats

--- src/components/Career.tsx
+++ src/components/Career.tsx
@@ -1,11 +1,12 @@
-import {Link} from "react-router-dom"
+import { Link } from "react-router-dom";
 
-
-export default function Career():JSX.Element {
+export default function Career(): JSX.Element {
   return (
     <div className="CareerPage">
       Career Page
-      <Link to="/" id="Link">Home</Link>
+      <Link to="/" id="Link">
+        Home
+      </Link>
     </div>
-  )
+  );
 }
--- src/components/X44.tsx
+++ src/components/X44.tsx
@@ -1,11 +1,12 @@
-import {Link} from "react-router-dom"
+import { Link } from "react-router-dom";
 
-export default function X44():JSX.Element {
+export default function X44(): JSX.Element {
   return (
-    
     <div className="X44Page">
       X44 Page
-      <Link to="/" id="Link">Home</Link>
+      <Link to="/" id="Link">
+        Home
+      </Link>
     </div>
-  )
+  );
 }
