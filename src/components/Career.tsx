536fffc697f036be09bec8630caed619d32b1841
Fola <jimiakinwale@icloud.com>
2022-08-08 20:03:45 +0100

    added all the content to the career page within a carousel, no styling done

--- src/components/Career.tsx
+++ src/components/Career.tsx
@@ -1,12 +1,21 @@
 import { Link } from "react-router-dom";
+import EarlyLife from "./careerPages/EarlyLife";
+import Karting from "./careerPages/Karting";
+import FRandF3 from "./careerPages/FRandF3";
+import Formula1 from "./careerPages/Formula1";
 
 export default function Career(): JSX.Element {
   return (
     <div className="CareerPage">
-      Career Page
       <Link to="/" id="Link">
         Home
       </Link>
+      <div className="CareerCarousel">
+        <EarlyLife />
+        <Karting />
+        <FRandF3 />
+        <Formula1 />
+      </div>
     </div>
   );
 }
---
+++ src/components/careerPages/FRandF3.tsx
@@ -0,0 +1,69 @@
+export default function FRandF3() {
+  return (
+    <div className="FRandF3">
+      <h1>Formula Renault and Formula Three</h1>
+      <section>
+        Hamilton began his car racing career in the 2001 British Formula Renault
+        Winter Series, finishing fifth in the standings. This led to a full 2002
+        Formula Renault UK campaign with Manor Motorsport in which he finished
+        third overall. He remained with Manor for another year, winning the
+        championship ahead of Alex Lloyd. Having clinched the championship,
+        Hamilton missed the last two races of the season to make his debut in
+        the season finale of the British Formula 3 Championship. In his first
+        race he was forced out with a puncture, and in the second he crashed out
+        and was taken to hospital after a collision with teammate Tor Graves.
+        <br />
+        <br />
+        Asked in 2002 about the prospect of becoming one of the youngest ever
+        Formula One drivers, Hamilton replied that his goal was &quot;not to be
+        the youngest in Formula One&quot; but rather &quot;to be experienced and
+        then show what I can do in Formula One&quot;. He made his debut with
+        Manor in the 2004 Formula 3 Euro Series, ending the year fifth in the
+        championship. He also won the Bahrain F3 Superprix and twice raced in
+        the Macau F3 Grand Prix. Williams had come close to signing Hamilton but
+        did not because BMW, their engine supplier at the time, would not fund
+        him. Hamilton eventually re-signed with McLaren. According to then
+        McLaren executive and future CEO Martin Whitmarsh, who was responsible
+        for guiding Hamilton through the team&apos;s young driver programme, he
+        and Anthony Hamilton had a &quot;huge row&quot; at the end of the
+        season, with Lewis&apos; father pushing for him to move up to GP2 for
+        2005, while Whitmarsh felt that he should remain in F3 for a second
+        season, culminating in Whitmarsh tearing up Lewis&apos; contract;
+        however, Lewis called Whitmarsh six weeks later and re-signed with the
+        team.
+        <br />
+        <br />
+        Hamilton first tested for McLaren in late 2004 at Silverstone. Hamilton
+        moved to the reigning Euro Series champions ASM for the 2005 season and
+        dominated the championship, winning 15 of the 20 rounds. He also won the
+        Marlboro Masters of Formula 3 at Zandvoort. After the season British
+        magazine Autosport featured him in their &quot;Top 50 Drivers of
+        2005&quot; issue, ranking Hamilton 24th.
+      </section>
+      <h1>GP2</h1>
+      <section>
+        Hamilton moved to ASM&apos;s sister GP2 team, ART Grand Prix, for the
+        2006 season. Hamilton won the GP2 championship at his first attempt,
+        beating Nelson Piquet Jr. and Timo Glock. He secured a dominant win at
+        the Nürburgring, despite a penalty for speeding in the pit lane. At his
+        home race at Silverstone, Hamilton overtook two rivals at Becketts, a
+        series of high-speed bends where overtaking is rare. In Istanbul he
+        recovered from a spin that left him in eighteenth place to take second.
+        Hamilton won the title in unusual circumstances, inheriting the final
+        point he needed after Giorgio Pantano was stripped of fastest lap in the
+        Monza feature race.
+        <br />
+        <br />
+        Hamilton&apos;s success in the GP2 championship coincided with a vacancy
+        at McLaren following the departure of Juan Pablo Montoya to NASCAR and
+        Kimi Räikkönen to Ferrari. After months of speculation on whether
+        Hamilton, Pedro de la Rosa or Gary Paffett would be paired with
+        defending champion Fernando Alonso for 2007, Hamilton was confirmed as
+        the team&apos;s second driver. He was told of McLaren&apos;s decision at
+        the end of September, but the news was not made public for almost two
+        months, for fear that it would be overshadowed by Michael
+        Schumacher&apos;s retirement announcement.
+      </section>
+    </div>
+  );
+}
