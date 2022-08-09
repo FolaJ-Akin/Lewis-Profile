100e6376f0e4b89db4e102d27157634952e4d422
Fola <jimiakinwale@icloud.com>
2022-08-09 19:20:16 +0100

    built live f1 standings page with axios requests to an Api

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import LandingPage from "./components/LandingPage";
 import Career from "./components/Career";
 import X44 from "./components/X44";
 import Navbar from "./Navbar";
+import F1Standings from "./components/F1Standings";
 
 export default function App(): JSX.Element {
   return (
@@ -12,6 +13,7 @@ export default function App(): JSX.Element {
           <Route path="/" element={<LandingPage />} />
           <Route path="/Career" element={<Career />} />
           <Route path="/X44" element={<X44 />} />
+          <Route path="/Standings" element={<F1Standings />} />
         </Routes>
       </div>
       <Navbar />
---
+++ src/components/F1Standings.tsx
@@ -0,0 +1,53 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { Data, driverStandingsData } from "../utils/interfaces";
+import DriverTile from "../utils/DriverTile";
+import { Link } from "react-router-dom";
+
+export default function F1Standings(): JSX.Element {
+  const [apiData, setApiData] = useState<Data>();
+  const [driverStandings, setDriverStandings] =
+    useState<driverStandingsData[]>();
+
+  useEffect(() => {
+    axios
+      .get(
+        "http://ergast.com/api/f1/current/driverStandings.json?callback=myParser"
+      )
+      .then((response) => {
+        const lastIndex = response.data.length;
+        const messyResponse = response.data.slice(9, lastIndex - 1);
+        const cleanResponse = JSON.parse(messyResponse);
+        setApiData(cleanResponse);
+      });
+  }, []);
+  useEffect(() => {
+    if (apiData !== undefined) {
+      const standings =
+        apiData.MRData.StandingsTable.StandingsLists[0].DriverStandings;
+      setDriverStandings(standings);
+    }
+  }, [apiData]);
+
+  return (
+    <div className="f1Standings">
+      <Link to="/" id="Link">
+        Home
+      </Link>
+      <h1>Live F1 Standings </h1>
+      <h2>Season: {apiData?.MRData.StandingsTable.season}</h2>
+      {driverStandings?.map((oneDriverData, index) => (
+        <DriverTile
+          position={oneDriverData.position}
+          currentPoints={oneDriverData.points}
+          racesWon={oneDriverData.wins}
+          driverFirstName={oneDriverData.Driver.givenName}
+          driverLastName={oneDriverData.Driver.familyName}
+          constructorName={oneDriverData.Constructors[0].name}
+          driverNumber={oneDriverData.Driver.permanentNumber}
+          key={index}
+        />
+      ))}
+    </div>
+  );
+}
